baa768488982bc9d495e04d88c80b974a63e3dfd
Erikas <erkexzcx@users.noreply.github.com>
2023-10-15 23:42:01 +0300

    add version

--- cmd/rrmultivoice/main.go
+++ cmd/rrmultivoice/main.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"rrmultivoice/pkg/rrmultivoice"
 	"time"
 )
 
 var (
-	//lint:ignore U1000 Ignore will be set by build command
 	version string
 
 	flagInterval  = flag.Duration("interval", 300*time.Millisecond, "Interval for scanning fds.")
 	flagSoundsDir = flag.String("soundsdir", "/opt/rockrobo/resources/sounds/en/", "Original directory from which robot plays voice-lines.")
 	flagPacksDir  = flag.String("packsdir", "/opt/rockrobo/resources/sounds/en/additional_sounds", "Directory of additional sound directories")
+	flagVersion   = flag.Bool("version", false, "prints version of the application")
 )
 
 func main() {
 	flag.Parse()
+
+	if *flagVersion {
+		fmt.Println("Version:", version)
+		return
+	}
 
 	// Ensure interval is not abnormally low
 	if *flagInterval < 10*time.Millisecond {
